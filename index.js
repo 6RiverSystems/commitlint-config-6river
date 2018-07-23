d535e6aa064b156e3ae942b930c4fa5e4c5c1356
Adam Vigneaux <adam@adamvig.com>
2018-07-23 15:12:14 -0400

    feat: downgrade subject-case rule to a warning

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ module.exports = {
 	],
 	rules: {
 		'header-max-length': [2, 'always', 120],
+		'subject-case': [
+			1,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+		],
 	},
 };
